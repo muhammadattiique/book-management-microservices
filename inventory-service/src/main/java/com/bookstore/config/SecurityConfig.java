bd3525ae49ecf70e21d3df34cacc69ac263f9d73
Muhammad Attique <muhammadattiquetariq@gmail.com>
2026-09-12 13:19:06 +0500

    Update SecurityConfig.java

--- inventory-service/src/main/java/com/bookstore/config/SecurityConfig.java
+++ inventory-service/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -23,7 +23,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/v1/inventory/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api/v1/inventory/**",
+                                "/actuator/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
